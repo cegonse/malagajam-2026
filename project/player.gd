887377a4d6b2f35506577005aa1cf5152b328c44
cegonse <contact@cesargonzalez.dev>
2026-01-31 10:10:23 +0100

    Horizontal movement on gravity change

--- project/player.gd
+++ project/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const horizontal_speed = 10.0
+const horizontal_speed = 100.0
 const jump_speed = 300.0
 
 func _process(delta: float) -> void:
@@ -15,21 +15,33 @@ func _process(delta: float) -> void:
 		rotate(rotation)
 		up_direction = up_direction.rotated(-rotation)
 
-func _physics_process(delta: float) -> void:
-	var gravity = (get_gravity() * delta).y
-	var gravity_vec = up_direction.rotated(PI) * gravity
-	var jump_vec = up_direction * jump_speed
+func handle_vertical_movement(delta: float) -> void:
+	var gravity_magnitude = (get_gravity() * delta).y
+	var gravity = up_direction.rotated(PI) * gravity_magnitude
+	var jump_velocity = up_direction * jump_speed
 	
 	if not is_on_floor():
-		velocity += gravity_vec
+		velocity += gravity
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity = jump_vec
+		velocity = jump_velocity
 
-	var horizontal_target = 0
-	if Input.is_action_pressed("move_left"):
-		horizontal_target = -1
-	elif Input.is_action_pressed("move_right"):
-		horizontal_target = 1
+func close_to(v1: Vector2, v2: Vector2) -> bool:
+	return v1.distance_to(v2) < 0.1
 
+func handle_horizontal_movement(delta: float) -> void:
+	var x_axis = Input.get_axis("move_left", "move_right")
+	var y_axis = Input.get_axis("move_down", "move_up")
+	
+	var should_move_horizontally = close_to(up_direction, Vector2.UP) or close_to(up_direction, Vector2.DOWN)
+	var should_move_vertically = close_to(up_direction, Vector2.LEFT) or close_to(up_direction, Vector2.RIGHT)
+	
+	if should_move_horizontally:
+		velocity.x = x_axis * horizontal_speed
+	elif should_move_vertically:
+		velocity.y = -y_axis * horizontal_speed
+
+func _physics_process(delta: float) -> void:
+	handle_vertical_movement(delta)
+	handle_horizontal_movement(delta)
 	move_and_slide()
