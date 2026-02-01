023588de44d7262b99213ba02b1465cb420e629f
cegonse <contact@cesargonzalez.dev>
2026-02-01 11:28:03 +0100

    Level 11 and 9 with graphics

---
+++ tiled/Tileset_Full.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset_Full" tilewidth="16" tileheight="16" tilecount="110" columns="11">
+ <image source="../project/sprites/Tileset_Full.png" width="176" height="160"/>
+</tileset>
